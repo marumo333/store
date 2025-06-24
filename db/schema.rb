8b3b0fdca9e13c523864e127d5bbbdf5fd451401
marumo333 <seito5026junior@icloud.com>
2025-06-24 11:16:37 +0900

    add:migrateコマンドでデータベースに適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_23_061207) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_23_133449) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
