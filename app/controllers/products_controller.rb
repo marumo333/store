125f13b12cbd00c76dc73d43dc640090a71acddd
marumo333 <seito5026junior@icloud.com>
2025-07-07 22:44:13 +0900

    コントローラにcreateアクションを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,17 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
