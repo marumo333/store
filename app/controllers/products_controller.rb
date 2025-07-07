b98e29892545e919a3c354a8453d356f7dfaeac3
marumo333 <seito5026junior@icloud.com>
2025-07-07 22:50:54 +0900

    edit/updateアクションを追加して、商品編集機能を作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,18 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @product = Product.find(params[:id])
+  end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
     def product_params
       params.expect(product: [ :name ])
